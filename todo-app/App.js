0e43bee8db9716f1891a1865f211f1cfb8d31639
pedro davim <pedrodavim@pedros-MBP-2.lan>
2019-06-27 13:50:26 +0100

    commit 2

---
+++ todo-app/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Main from './app/components/Main.js'
+
+export default class app extends Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
+
